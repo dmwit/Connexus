b665f57dc753941130498bba8ecdda950d1279ac
Daniel Wagner <daniel@wagner-home.com>
2010-05-22 19:11:30 -0400

    configurable width and height as command-line parameters

--- Main.hs
+++ Main.hs
@@ -12,6 +12,7 @@ import Data.Default
 import Data.IORef
 import Graphics.UI.Gtk hiding (Viewport, viewportNew)
 import Graphics.UI.Gtk.Gdk.EventM (MouseButton(..))
+import System.Environment
 -- }}}
 clickGrid gridRef lockRef b x y = case b of
 	LeftButton   -> doRotation counterclockwise
@@ -31,18 +32,22 @@ drawGridLock gridRef lockRef = do
 
 main = do
 	initGUI
+	args    <- getArgs
+	let (w, h) = case map reads args of
+		[[(w, "")], [(h, "")]] -> (w, h)
+		_ -> (11, 11)
 	window  <- windowNew
-	grid    <- evalStateT (randomGrid 11 11) def
+	grid    <- evalStateT (randomGrid w h) def
 	gridRef <- newIORef grid
 	lockRef <- newIORef def
-	ioStateT gridRef $ rotateGridRandomly >> time >>= signal (5, 5)
+	ioStateT gridRef $ rotateGridRandomly >> time >>= signal (w `div` 2, h `div` 2)
 	da      <- viewportNew def {
 		stabilizationTime = liftM (maybe Already ExactTime . stable) (readIORef gridRef),
 		draw     = drawGridLock gridRef lockRef,
 		click    = clickGrid    gridRef lockRef,
 		position = Position {
-			centerX = def { dimension =  5 }, centerY = def { dimension =  5 },
-			width   = def { dimension = 11 }, height  = def { dimension = 11 }
+			centerX = def { dimension = (fromIntegral w - 1) / 2 }, centerY = def { dimension = (fromIntegral h - 1) / 2 },
+			width   = def { dimension =  fromIntegral w          }, height  = def { dimension =  fromIntegral h          }
 			}
 		}
 	set window [containerChild := da]
@@ -58,3 +63,4 @@ main = do
 -- * don't lock off-grid locations
 -- * make it easier to change the size of the grid
 -- * keyboard bindings
+-- * non-idiot mode: don't face edges outside
