94d271d522a72b1c0320a616600601273de108b0
Daniel Wagner <daniel@wagner-home.com>
2012-06-09 17:06:32 -0400

    a module with all the different rounding strategies

---
+++ Rounding.hs
@@ -0,0 +1,27 @@
+module Rounding where
+
+{-# INLINE towardZero          #-}
+{-# INLINE towardInf           #-}
+{-# INLINE towardNegInf        #-}
+{-# INLINE awayFromZero        #-}
+{-# INLINE nearestTowardZero   #-}
+{-# INLINE nearestTowardInf    #-}
+{-# INLINE nearestTowardNegInf #-}
+{-# INLINE nearestAwayFromZero #-}
+{-# INLINE nearestBanker       #-}
+
+towardZero     = truncate
+towardInf      = ceiling
+towardNegInf   = floor
+awayFromZero v = if v > 0 then ceiling v else floor v
+
+nearestTowardZero   v = if isHalf v then towardZero   v else round v
+nearestTowardInf    v = if isHalf v then towardInf    v else round v
+nearestTowardNegInf v = if isHalf v then towardNegInf v else round v
+nearestAwayFromZero v = if isHalf v then awayFromZero v else round v
+nearestBanker         = round
+
+{-# INLINE fi #-}
+{-# INLINE isHalf #-}
+fi = fromIntegral
+isHalf v = v - fromInteger (towardNegInf v) == 0.5
